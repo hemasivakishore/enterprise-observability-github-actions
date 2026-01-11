1461467923378f4e3ac5ab217032898ddfac78ef
hemasivakishore <hemasivakishore@gmail.com>
2026-01-11 21:17:14 +0530

    target-nodes provisioned and working fine

--- target-nodes/target-nodes.tf
+++ target-nodes/target-nodes.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "target-node" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = ["aws_security_group.observability-sg"]
-  key_name               = "vhsk"
+  vpc_security_group_ids = [aws_security_group.observability-sg.id]
+  key_name = "vhsk"
 
   tags = {
     Name = "Target-Node-0${count.index + 1}"
